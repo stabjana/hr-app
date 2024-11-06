46bca6c9af27920a2bb9328f44653097a8bf6cbb
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2024-11-06 13:49:07 +0200

    add login/ logout button

--- src/components/EmployeeList.jsx
+++ src/components/EmployeeList.jsx
@@ -1,14 +1,34 @@
 import Card from "./EployeeCard";
 import employees from "./EmployeesData";
 import './employee.css'
+import { useState } from 'react';
+
 
 function List() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const handleLogin = () => {
+        setIsLoggedIn(!isLoggedIn);
+    };
+
     return (
         <main>
-            <h2>Employee List</h2>
-            <div className="employeeList">
-                {employees.map((el) => < Card key={el.id} name={el.name} role={el.role} department={el.department} startDate={el.startDate} location={el.location} emergencyContact={el.emergencyContact} trainings={el.trainings} performanceGrade={el.performanceGrade} ></Card>)};
-            </div>
+            {isLoggedIn ? (
+                <div>
+                    <h2>Employee List</h2>
+                    <div className="employeeList">
+                        {employees.map((el) => < Card key={el.id} name={el.name} role={el.role} department={el.department} startDate={el.startDate} location={el.location} emergencyContact={el.emergencyContact} trainings={el.trainings} performanceGrade={el.performanceGrade} ></Card>)};
+                        <button onClick={handleLogin}>Log out</button>
+                    </div>
+                </div>
+
+            ) : (
+                <div>
+                    <h2>Please log in to see the Employees</h2>
+                    <button onClick={handleLogin}>Log in</button>
+                </div>
+            )
+            }
         </main >
     );
 };
