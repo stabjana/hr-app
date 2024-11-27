65157cef5594d31b53a65a8d744e3f82cbefb7d9
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2024-11-27 13:53:35 +0200

    add useState and useEffect to List.jsx

--- src/components/EmployeeList/EmployeeList.jsx
+++ src/components/EmployeeList/EmployeeList.jsx
@@ -1,15 +1,31 @@
 import EmployeeCard from "../EmployeeCard/EployeeCard.jsx";
 import "./employee.css";
+import { useState, useEffect } from "react";
+
 /* import { employeesData } from "../../data/EmployeesData.jsx"; */
 
 const EmployeeList = () => {
+    const [posts, setPosts] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
+    useEffect(() => {
+        fetch('http://localhost:3002/')
+            .then((response) => response.json())
+            .then((data) => {
+                setPosts(data);
+                setIsLoading(false);
+            });
+    });
 
     return (
         <div className="list">
-            {employeesData.map((employee) => {
-                return <EmployeeCard key={employee.id} {...employee} />;
-            })}
+            {isLoading ? (
+                <p>Page is loading...</p>
+            ) : (
+                posts.map((employee) => {
+                    return <EmployeeCard key={employee.id} {...employee} />
+                })
+            )}
         </div>
     );
 };
