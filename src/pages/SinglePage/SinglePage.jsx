aae0b232ca121dd3a385487a7f7c941769b07568
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2024-12-12 20:52:53 +0200

    fix button import

--- src/pages/SinglePage/SinglePage.jsx
+++ src/pages/SinglePage/SinglePage.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import useAxiosRequest from "../../utilities/useAxios";
 import { useEmployeeStatus } from "../../hooks/useEmployeeStatus";
-import Button from "../components/Button/Button";
+import Button from "../../components/Button/Button";
 import styles from "./SinglePage.module.css";
 
 const SinglePage = () => {
