ea5260dcba96e05c81e1641aa7a7e25e250cc862
Jana Stefanie <s2400789@BCCWRW4ietokone.fritz.box>
2024-12-27 23:14:22 +0100

    add error page content and Back button

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,6 +1,11 @@
+import Button from "../components/Button/Button";
+import { useNavigate } from "react-router-dom";
+
 const ErrorPage = () => {
+  const navigate = useNavigate();
   return <div>
-    <p>Something went wrong! Don't panic, we will work on a solution. Maybe...</p>
+    <h1>Page not found</h1>
+    <p>Sorry, we couldn't find the page you're looking for. Please try again or contact the support.</p>
     <Button text="Back" role="primary-light" onClick={() => navigate(-1)} />
   </div>;
 };
